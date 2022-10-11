8a42a56565538785e1c0f3bed202dd22dd7c2b84
aravindsabu <aravindsabu2001@gmail.com>
2022-10-11 22:35:00 +0530

    on

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { DataComponent } from './data/data.component';
+import { RouterModule, Routes } from '@angular/router';
+const appRoutes:Routes=[
+  {
+  path:"",component:DataComponent
+}
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
